65d60aef50eff5b92350f46113c9eaad5ddf7421
germansuarez84 <germancloud@gmail.com>
2021-10-14 17:25:29 -0500

    Diseño y binding

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NumericValueAccessor } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  factor1:number;
+  factor2:number;
+  operation:number;
+  soperation:string;
 
-  constructor() {}
+
+  constructor() {
+    this.setQuestion();
+
+  }
+
+  setQuestion(){
+    this.factor1=this.getRandomInt(1,1000);
+    this.factor2=this.getRandomInt(1,1000);
+    this.operation=this.getRandomInt(1,5);
+    switch(this.operation){
+      case 1: this.soperation="+";break;
+      case 2: this.soperation="-";break;
+      case 3: this.soperation="*";break;
+      case 4: this.soperation="/";break;
+
+    }
+
+  }
+  
+  getRandomInt(min,max){
+    return Math.floor(Math.random()*(max-min))+min;
+  }
+
 
 }
